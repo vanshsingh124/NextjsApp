d2de16ca0a8fff5b392c5c40a50ae9d42c17db58
Vansh Singh <vansh9682010@gmail.com>
2023-01-06 16:32:26 +0530

    Second Commit

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -21,7 +21,7 @@ function Navbar(props) {
       <nav className={`"navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} "`} style={co}>
         <div className="container-fluid">
 
-          <a className="navbar-brand" href="/" style={n}>{props.title}</a>
+          <Link className="navbar-brand" href="/" style={n}>{props.title}</Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
